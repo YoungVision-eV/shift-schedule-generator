6c6402f2c23155209de1d352d1a5b4ef9466cffd
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-04-21 14:28:28 +0200

    inquire

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ mod schedule_table;
 fn main() {
     let args= parser::parse_args();
     let schedule = fill_schedule(&args.1, &args.0);
-    print_schedule_html(&schedule);
+    print_schedule_md(&schedule);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,13 @@
-use clap::Parser;
+use std::process::exit;
+
+use clap::{error, Parser};
 use shift_schedule_generator::Shift;
 
 #[derive(clap::Parser)]
 struct Cli {
     names: Vec<String>,
     #[arg(short, long)]
-    days: usize,
+    days: Option<usize>,
 }
 
 pub fn parse_args() -> (Vec<String>, Vec<Vec<Shift>>) {
@@ -37,13 +39,32 @@ pub fn parse_args() -> (Vec<String>, Vec<Vec<Shift>>) {
     ];
     let start = inquire::DateSelect::new("Pick a start date").prompt();
     let end = inquire::DateSelect::new("Pick a end date").prompt();
-    let days;
-    match (start, end) {
-        (Ok(start), Ok(end)) => days = (end-start).num_days() + 1,
-        default => days = 0,
+    let days = match (start, end) {
+        (Ok(start), Ok(end)) => (end - start).num_days() + 1,
+        default => 0,
+    };
+    if days <= 0 {
+        eprintln!("Invalid Dates");
+        exit(1);
+    }
+    let prompt_n_shifts = inquire::CustomType::<usize>::new("How many shifts per day?")
+        .with_error_message("Please type a valid number")
+        .prompt();
+    let n_shifts = match prompt_n_shifts {
+        Ok(n) => n,
+        Err(_) => {
+            eprintln!("Error");
+            exit(1);
+        }
+    };
+    for _ in 0..n_shifts {
+        let shift_title = inquire::Text::new("Title of the shift:").prompt();
+        let n_people = inquire::CustomType::<usize>::new("How many persons?")
+            .with_error_message("Please type a valid number")
+            .prompt();
     }
 
     let args = Cli::parse();
-    
+
     (args.names, vec![day; days as usize])
 }
