e9e64c154b809f7ffc0a75b0425a9b3f22e27db6
hannes <hannes.rother@online.de>
2025-05-05 22:52:59 +0200

    add prompt to disable shifts (WIP)

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use std::process::exit;
 
 use clap::{error, Parser};
+use inquire::{CustomType, DateSelect, MultiSelect, Text};
 use shift_schedule_generator::Shift;
 
 #[derive(clap::Parser)]
@@ -8,6 +9,25 @@ struct Cli {
     names: Vec<String>,
     #[arg(short, long)]
     days: Option<usize>,
+}
+
+fn prompt_disabled_shifts(n_days: usize, n_shifts: usize) {
+    let all_days = (1..n_days).collect();
+    let ans = MultiSelect::new("Select days one which you want to disable shifts: ", all_days).prompt();
+    let selected_days = match ans {
+        Ok(d) => d,
+        Err(_) => todo!("unhandled error"),
+    };
+    let shifts_per_day: Vec<_> = (1..n_shifts).collect();
+    let shift_choices: Vec<_> = selected_days.iter().flat_map(|d| {
+        shifts_per_day.iter().map(move |s| format!("Day {}: Shift {}", d, s))
+    }).collect();
+    let ans = MultiSelect::new("Select shifts to disable.", shift_choices).prompt();
+    let selected_shifts = match ans {
+        Ok(s) => s,
+        Err(_) => todo!("unhandled error"),
+    };
+    println!("{:?}", selected_shifts);
 }
 
 pub fn parse_args() -> (Vec<String>, Vec<Vec<Shift>>) {
@@ -37,8 +57,8 @@ pub fn parse_args() -> (Vec<String>, Vec<Vec<Shift>>) {
             people: 2,
         },
     ];
-    let start = inquire::DateSelect::new("Pick a start date").prompt();
-    let end = inquire::DateSelect::new("Pick a end date").prompt();
+    let start = DateSelect::new("Pick a start date").prompt();
+    let end = DateSelect::new("Pick a end date").prompt();
     let days = match (start, end) {
         (Ok(start), Ok(end)) => (end - start).num_days() + 1,
         default => 0,
@@ -47,7 +67,7 @@ pub fn parse_args() -> (Vec<String>, Vec<Vec<Shift>>) {
         eprintln!("Invalid Dates");
         exit(1);
     }
-    let prompt_n_shifts = inquire::CustomType::<usize>::new("How many shifts per day?")
+    let prompt_n_shifts = CustomType::<usize>::new("How many shifts per day?")
         .with_error_message("Please type a valid number")
         .prompt();
     let n_shifts = match prompt_n_shifts {
@@ -58,11 +78,12 @@ pub fn parse_args() -> (Vec<String>, Vec<Vec<Shift>>) {
         }
     };
     for _ in 0..n_shifts {
-        let shift_title = inquire::Text::new("Title of the shift:").prompt();
-        let n_people = inquire::CustomType::<usize>::new("How many persons?")
+        let shift_title = Text::new("Title of the shift:").prompt();
+        let n_people = CustomType::<usize>::new("How many persons?")
             .with_error_message("Please type a valid number")
             .prompt();
     }
+    prompt_disabled_shifts(days as usize, n_shifts);
 
     let args = Cli::parse();
 
